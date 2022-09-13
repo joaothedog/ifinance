b75879ecc941a127b7491caad47b9681e0f2cfb7
João Oliveira <jvcbatist4@outlook.com>
2022-09-13 17:05:20 -0300

    Estilização inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as Component from './App.styles';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Component.Container>
+      <Component.Header>
+        <Component.HeaderText>iFinance</Component.HeaderText>
+      </Component.Header>
+
+      <Component.Body>...</Component.Body>
+    </Component.Container>
   );
-}
+};
 
 export default App;
